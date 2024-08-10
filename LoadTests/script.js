8fec35a63a9dc7508437545be82a329ae549f352
Andrii Borkovych <andrii.borkovich@nerdysoft.com>
2024-08-10 12:54:11 +0300

    moved host inside test script

--- LoadTests/script.js
+++ LoadTests/script.js
@@ -1,31 +1,30 @@
-import http from 'k6/http';
-import exec from 'k6/execution';
-import { check, sleep } from 'k6';
+import http from "k6/http";
+import exec from "k6/execution";
+import { check, sleep } from "k6";
 
-const baseUrl = __ENV.BASE_URL;
+const baseUrl = "localhost:5000";
 
 export let options = {
-    stages: [
-        { duration: '30s', target: 50 }, // Ramp-up to 50 users over 30 seconds
-        { duration: '30s', target: 0 },  // Ramp-down to 0 users over 30 seconds
-    ],
-    thresholds: {
-        http_req_duration: ['p(95)<5000'], // 95% of requests should be below 5s
-    },
-    cloud: {
-        // Project: Default project
-        projectID: 3706889,
-        // Test runs with the same name groups test runs together.
-        name: 'DemoTest'
-      }
+  stages: [
+    { duration: "30s", target: 50 }, // Ramp-up to 50 users over 30 seconds
+    { duration: "30s", target: 0 }, // Ramp-down to 0 users over 30 seconds
+  ],
+  thresholds: {
+    http_req_duration: ["p(95)<5000"], // 95% of requests should be below 5s
+  },
+  cloud: {
+    // Project: Default project
+    projectID: 3706889,
+    // Test runs with the same name groups test runs together.
+    name: "DemoTest",
+  },
 };
 
 export default function () {
-
-    let res = http.get(`http://${baseUrl}/WeatherForecast`);
-    check(res, {
-        'status is 200': (r) => r.status === 200,
-        'response time is less than 5s': (r) => r.timings.duration < 5000,
-    });
-    sleep(1); // Wait for 1 second between iterations
+  let res = http.get(`http://${baseUrl}/WeatherForecast`);
+  check(res, {
+    "status is 200": (r) => r.status === 200,
+    "response time is less than 5s": (r) => r.timings.duration < 5000,
+  });
+  sleep(1); // Wait for 1 second between iterations
 }
