33b1e09d775c24d01a5a722fe7e716c84c832303
andrii-nerdysoft <142494764+AndriiBorkovych@users.noreply.github.com>
2024-08-10 14:48:52 +0300

    added rate limit rules (#1)
    * fix load test
    
    * removed global env
    
    * added sleep stage
    
    * changed path
    
    * restructure
    
    * configured rate limiters, add secrets for ci in grafana cloud
    
    * removed docker build
    
    * moved env variable
    
    * moved host inside test script
    
    * added cors
    
    * removed sleep from workflow
    
    * added serilog

--- RateLimitersDemo/Program.cs
+++ RateLimitersDemo/Program.cs
@@ -1,15 +1,67 @@
+using System.Threading.RateLimiting;
+using Serilog;
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddSerilog((services, lc) => lc.ReadFrom.Configuration(builder.Configuration));
+
+builder.Services.AddRateLimiter(rateLimiterOptions =>
+{
+    rateLimiterOptions.GlobalLimiter = PartitionedRateLimiter.CreateChained(
+         PartitionedRateLimiter.Create<HttpContext, string>(
+            httpContext => RateLimitPartition.GetConcurrencyLimiter(
+                partitionKey: httpContext.Connection.RemoteIpAddress?.ToString(),
+                factory: _ => new ConcurrencyLimiterOptions
+                {
+                    PermitLimit = 1,
+                    QueueLimit = 5,
+                    QueueProcessingOrder = QueueProcessingOrder.OldestFirst
+                })),
+         PartitionedRateLimiter.Create<HttpContext, string>(
+            httpContext => RateLimitPartition.GetFixedWindowLimiter(
+                partitionKey: httpContext.Connection.RemoteIpAddress?.ToString(),
+                factory: _ => new FixedWindowRateLimiterOptions
+                {
+                    PermitLimit = 100,
+                    Window = TimeSpan.FromMinutes(1)
+                })));
+
+    rateLimiterOptions.AddPolicy("registration", httpContext =>
+        RateLimitPartition.GetFixedWindowLimiter(
+            partitionKey: httpContext.Connection.RemoteIpAddress?.ToString(),
+            factory: _ => new FixedWindowRateLimiterOptions
+            {
+                PermitLimit = 5,
+                Window = TimeSpan.FromMinutes(1)
+            }));
+
+    rateLimiterOptions.OnRejected = async (context, cancellationToken) =>
+    {
+        if (context.Lease.TryGetMetadata(MetadataName.RetryAfter, out var retryAfter))
+        {
+            context.HttpContext.Response.Headers.RetryAfter = retryAfter.TotalSeconds.ToString();
+        }
+
+        context.HttpContext.Response.StatusCode = StatusCodes.Status429TooManyRequests;
+
+        context.Lease.TryGetMetadata(MetadataName.ReasonPhrase, out var reasonPhrase);
+        reasonPhrase ??= "Fixed window limit was reached";
+
+        context.HttpContext.RequestServices.GetService<ILoggerFactory>()?
+            .CreateLogger("Microsoft.AspNetCore.RateLimitingMiddleware")
+            .LogWarning("Rejected request: {EndpointName}, reason: {ReasonPhrase}", context.HttpContext.Request.Path, reasonPhrase);
+
+        await context.HttpContext.Response.WriteAsync("Too many requests. Please try again later.", cancellationToken);
+    };
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseSerilogRequestLogging();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -21,5 +73,11 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors(builder =>
+        builder
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader());
 
 app.Run();
