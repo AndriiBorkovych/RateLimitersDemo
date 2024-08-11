a8959ee64d2200ca99554a539201042fe5e84a45
Andrii Borkovych <andrii.borkovich@nerdysoft.com>
2024-08-11 13:50:09 +0300

    compeleted product endpoints

--- RateLimitersDemo/Controllers/ProductsController.cs
+++ RateLimitersDemo/Controllers/ProductsController.cs
@@ -11,13 +11,26 @@ namespace RateLimitersDemo.Controllers;
 [Route("api/[controller]")]
 public class ProductsController(ApplicationDbContext context) : ControllerBase
 {
-    [HttpPost]
+    [HttpPost("Create")]
     public async Task<IActionResult> Create(Product product)
     {
         context.Products.Add(product);
         await context.SaveChangesAsync();
 
         return CreatedAtAction(nameof(Create), new { id = product.Id }, product);
+    }
+
+    [HttpGet("GetById/{id}")]
+    [EnableRateLimiting(PolicyConstants.Fixed)]
+    public async Task<ActionResult<Product>> GetById(int id)
+    {
+        var product = await context.Products.FirstOrDefaultAsync(p => p.Id == id);
+        if (product is null)
+        {
+            return NotFound();
+        }
+
+        return product;
     }
 
     [HttpGet("All")]
@@ -28,7 +41,7 @@ public class ProductsController(ApplicationDbContext context) : ControllerBase
     }
 
     [HttpGet("Paid")]
-    [EnableRateLimiting(PolicyConstants.Fixed)]
+    [EnableRateLimiting(PolicyConstants.Concurrent)]
     public async Task<ActionResult<IEnumerable<Product>>> GetPaid()
     {
         return await context.Products.Where(p => p.IsPaid).ToListAsync();
@@ -41,11 +54,30 @@ public class ProductsController(ApplicationDbContext context) : ControllerBase
         return await context.Products.Where(p => !p.IsPaid).ToListAsync();
     }
 
+    [HttpPost("Pay/{id}")]
+    public async Task<IActionResult> Pay(int id)
+    {
+        var product = await context.Products.FirstOrDefaultAsync(p => p.Id == id);
+        if (product is null)
+        {
+            return NotFound();
+        }
+
+        if (product.IsPaid)
+        {
+            return BadRequest("Product is already paid");
+        }
+
+        product.IsPaid = true;
+
+        return NoContent();
+    }
+
     [HttpDelete("{id}")]
     public async Task<IActionResult> Delete(int id)
     {
         var product = await context.Products.FindAsync(id);
-        if (product == null)
+        if (product is null)
         {
             return NotFound();
         }
